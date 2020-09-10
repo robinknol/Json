89df9a53c0fbb822d84a3f2fcab90dfe62b71695
Robin Knol <robin@knol.email>
2020-09-10 16:21:49 +0200

    Initial commit

---
+++ ajax_callback.js
@@ -0,0 +1,56 @@
+function makeAjaxCall(url, methodType)
+{
+    let promiseObj = new Promise(function(resolve, reject)
+    {
+        console.log(url); //debug
+        let xmlhttp = new XMLHttpRequest();
+        xmlhttp.open(methodType, url, true);
+        xmlhttp.send();
+        xmlhttp.onreadystatechange = function()
+        {
+            if (xmlhttp.readyState == 4)
+            {
+                if (xmlhttp.status == 200)
+                {
+                    console.log("xmlhttp done successfully");
+                    let serverResponse = xmlhttp.responseText; //server antwoord met string
+                    console.log(serverResponse);
+                    resolve(serverResponse); //wordt via return promiseObj teruggeven
+                }
+                else
+                {
+                    reject(xmlhttp.status);
+                    console.log("xmlhttp failed");
+                }
+            }
+            else
+            {
+                console.log("xmlhttp failed");
+            }
+        }
+        console.log("request sent succesfully");
+    });
+    return promiseObj;
+}
+
+function errorHandler(statusCode)
+{
+    console.log("failed with status", status);
+}
+
+function getWeather2()
+{
+    makeAjaxCall(url, "GET"). then (showWeather2, errorHandler);
+}
+
+function showWeather2(JSONresponseFromAjax)
+{
+    let weatherObject = JSON.parse(JSONresponseFromAjax); //convert Json string => Object
+    let completeData = ""; //maak een lege string
+    for (const [key, value] of Object.defineProperties(weatherObject.liveweer[0]))
+    {
+        console.log(`${key}: ${value}`);
+        completeData += key + " : " + value +"<br>";
+        result.innerHTML = completeData; //string printen naar browser
+    }
+}
---
+++ script.js
@@ -0,0 +1,24 @@
+let activate1 = document.getElementById("raw"); //binds through DOM
+let activate2 = document.getElementById("formatted"); //binds through DOM and JS
+let activate3 = document.getElementById("wanted"); //binds through DOMbutton and JS
+let result = document.getElementById("result");
+activate1.addEventListener("click", getWeather1);
+// activate2.addEventListener("click", getWeather2);
+// activate3.addEventListener("click", getWeather3);
+
+// weather API http://weerlive.nl/delen.php
+let apiAdress = "http://weerlive.nl/api/json-data-10min.php?key="; //adress
+let key = "demo";
+let locatie = "&locatie=";
+let geoLocation = "Amsterdam"; //Location as string
+let url = apiAdress + key + locatie + geoLocation; //url of the API
+
+function getWeather1()
+{
+    console.log(url);
+    makeAjaxCall(url, "GET"). then (showWeather1, errorHandler);
+}
+function showWeather1(JSONresponseFromAjax)
+{
+    result.innerHTML = JSONresponseFromAjax; //Shows the raw data
+}
